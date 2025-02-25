74b55be9671a7bec876afc95bc4273243feb3ff0
Mayank Chugh <mayank.chugh@automattic.com>
2025-02-25 13:23:09 +0000

    init commit
    Add normal dist
    
    correct the calc of normal random
    
    delete html

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # beta-random-sandbox-js
+Plots chart to compare the beta random number generation using two methods:
+1. Using @stdlib/random-base-beta
+2. Using simple mathematics of calculating via Random Gamma method where random numbers required for calculation are generated using Math.random()
+3. Using simple mathematics of calculating via Random Gamma method where random numbers required for calculation of the same are generated from a normal distribution
+
+The @stdlib/random-base-beta uses "Algorithm BB by Cheng".
+
+Random Gamma method: https://en.wikipedia.org/wiki/Beta_distribution#Random_variate_generation
+
+
+## How to use
+
+### Install deps
+`npm install`
+
+### Specify Parameters
+Takes three parameters - 
+Shape parameters: `alpha` and `beta`
+Number of random numbers to be generated for analysis: `n`
+
+Specify in the top of file `index.js`
+
+```
+...deps
+
+// Parameters for the beta distribution
+const alpha = 10;
+const beta = 64000;
+
+// How many random numbers to be genereate
+const n = 50000;
+
+... rest of code
+```
+
+Run the script using 
+`node index.js`
+
+### How to visualise
+
+Running the script will open an html file with three series:
+1. Beta distribution
+2. Random std numbers - using stdlib
+3. Random raw numbers uniform - using simple method of Random Gamma using Math.random()
+4. Random raw numbers normal - using simple method of Random Gamma using normal distribution
+
+Use the interactive plot to visualise the range of random numbers generated via both methods. the chart can be zoomed in to view in detail
---
+++ index.js
@@ -0,0 +1,195 @@
+const Beta = require("@stdlib/stats-base-dists-beta-ctor");
+const randomBeta = require("@stdlib/random-base-beta");
+const fs = require("fs");
+const { exec } = require("child_process"); // Import child_process module
+
+// Parameters for the beta distribution
+const alpha = 15342;
+const beta = 1000000;
+
+// How many random numbers to be genereate
+const n = 50000;
+
+/**
+ * Generates beta distribution using stdlib package
+ * @param {*} alpha
+ * @param {*} beta
+ * @returns
+ */
+function generateBetaDistribution(alpha, beta) {
+  const betaDist = new Beta(alpha, beta);
+  return betaDist;
+}
+
+/**
+ * Generates n random numbers from beta distribution using stdlib package
+ * @param {*} alpha
+ * @param {*} beta
+ * @param {*} n
+ */
+function generateBetaRandomUsingStdlib(alpha, beta, n) {
+  const randomStdlibNumbers = [];
+  for (let i = 0; i < n; i++) {
+    const rng = randomBeta(alpha, beta);
+    randomStdlibNumbers.push(rng);
+  }
+  return randomStdlibNumbers;
+}
+
+function standard_gamma_raw_uniform(shape) {
+  let d = shape - 1 / 3;
+  let c = 1 / Math.sqrt(9 * d);
+
+  while (true) {
+    let x, v;
+    do {
+      x = Math.random() * 2 - 1;
+      v = 1 + c * x;
+    } while (v <= 0);
+
+    v = v * v * v;
+    let u = Math.random();
+
+    if (u < 1 - 0.0331 * x * x * x * x) return d * v;
+    if (Math.log(u) < 0.5 * x * x + d * (1 - v + Math.log(v))) return d * v;
+  }
+}
+
+// Standard Normal variate using Box-Muller transform.
+function gaussianRandom(mean = 0, stdev = 1) {
+  const u = 1 - Math.random(); // Converting [0,1) to (0,1]
+  const v = Math.random();
+  const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+  // Transform to the desired mean and standard deviation:
+  return z * stdev + mean;
+}
+
+function standard_gamma_raw_normal(shape) {
+  let d = shape - 1 / 3;
+  let c = 1 / Math.sqrt(9 * d);
+
+  while (true) {
+    let x, v;
+    do {
+      x = gaussianRandom() * 2 - 1;
+      v = 1 + c * x;
+    } while (v <= 0);
+
+    v = v * v * v;
+    let u = Math.random();
+
+    if (u < 1 - 0.0331 * x * x * x * x) return d * v;
+    if (Math.log(u) < 0.5 * x * x + d * (1 - v + Math.log(v))) return d * v;
+  }
+}
+
+function generateBetaRandomUsingRandomGammaUniform(alpha, beta, n) {
+  const randomNumbers = [];
+  for (let i = 0; i < n; i++) {
+    const gamma1 = standard_gamma_raw_uniform(alpha);
+    const gamma2 = standard_gamma_raw_uniform(beta);
+    randomNumbers.push(gamma1 / (gamma1 + gamma2));
+  }
+  return randomNumbers;
+}
+
+function generateBetaRandomUsingRandomGammaNormal(alpha, beta, n) {
+  const randomNumbers = [];
+  for (let i = 0; i < n; i++) {
+    const gamma1 = standard_gamma_raw_normal(alpha);
+    const gamma2 = standard_gamma_raw_normal(beta);
+    randomNumbers.push(gamma1 / (gamma1 + gamma2));
+  }
+  return randomNumbers;
+}
+
+function generateBetaDistributionPlotPoints(alpha, beta) {
+  const betaDist = generateBetaDistribution(alpha, beta);
+  const xValues = [];
+  const yValues = [];
+  for (let x = 0; x <= 1; x += 0.0001) {
+    xValues.push(x);
+    yValues.push(betaDist.pdf(x));
+  }
+  return { xValues, yValues };
+}
+
+function plotRandomNumberComparison(alpha, beta, n) {
+  const betaDistributionPoints = generateBetaDistributionPlotPoints(
+    alpha,
+    beta
+  );
+  const randomStdlibNumbers = generateBetaRandomUsingStdlib(alpha, beta, n);
+  const randomRawNumbersUniform = generateBetaRandomUsingRandomGammaUniform(
+    alpha,
+    beta,
+    n
+  );
+  const randomRawNumbersNormal = generateBetaRandomUsingRandomGammaNormal(
+    alpha,
+    beta,
+    n
+  );
+  const createHtmlContent = `
+    <html>
+    <head>
+      <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+    </head>
+    <body>
+      <div id="plot"></div>
+      <script>
+        const xValues = ${JSON.stringify(betaDistributionPoints.xValues)};
+        const yValues = ${JSON.stringify(betaDistributionPoints.yValues)};
+        const randomNumbers = ${JSON.stringify(randomStdlibNumbers)};
+        const randomRawNumbersUniform = ${JSON.stringify(
+          randomRawNumbersUniform
+        )};
+        const randomRawNumbersNormal = ${JSON.stringify(
+          randomRawNumbersNormal
+        )};
+        const data = [
+          {
+            x: xValues,
+            y: yValues,
+            type: 'scatter',
+            mode: 'lines',
+            name: 'Beta Distribution'
+          },
+          {
+            x: randomNumbers,
+            type: 'histogram',
+            name: 'Random Numbers'
+          },
+          {
+            x: randomRawNumbersUniform,
+            type: 'histogram',
+            name: 'Random Raw Numbers Uniform'
+          },
+          {
+            x: randomRawNumbersNormal,
+            type: 'histogram',
+            name: 'Random Raw Numbers Normal'
+          }
+        ];
+        Plotly.newPlot('plot', data);
+      </script>
+    </body>
+    </html>
+    `;
+  return createHtmlContent;
+}
+
+const htmlContent = plotRandomNumberComparison(alpha, beta, n);
+// Write the HTML content to a file
+fs.writeFileSync("beta-distribution.html", htmlContent, "utf8");
+
+console.log("HTML file created: beta-distribution.html");
+
+// Open the HTML file in the default web browser
+exec("open beta-distribution.html", (error) => {
+  if (error) {
+    console.error("Failed to open the HTML file:", error);
+  } else {
+    console.log("HTML file created and opened: beta-distribution.html");
+  }
+});
